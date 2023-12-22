8ce15356d7125e5a338b2a4713cb526f939fdd6c
Sirisha Ongole <ongole.sirisha@gmail.com>
2023-12-21 19:32:30 -0500

    Initial Commit

---
+++ src/test/java/vk/automate/inline/InlineApplicationTests.java
@@ -0,0 +1,13 @@
+package vk.automate.inline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
